95d5dfc2fdcde06f0aa008c1eedd2f08d0b39a21
Dan Streetman <ddstreet@ieee.org>
2013-12-10 20:47:44 -0500

    update overcommit test with various changes

--- overcommit/overcommit_test.c
+++ overcommit/overcommit_test.c
@@ -8,11 +8,10 @@
 
 #define VERSION 1
 
-#define WORK_SLEEP_TIME 5
 #define MAX_USED_MEM 120
-#define RECENT_PAGES 4096
 #define MEM_INC_PCT 1
 
+static int work_sleep_time = 5;
 static int no_cpu = 0, no_random = 0, no_recent = 0;
 static int cpu_workers_exit = 0, mem_random_exit = 0, mem_recent_exit = 0;
 static int cpu_workers_pause = 0, mem_random_pause = 0, mem_recent_pause = 0;
@@ -21,6 +20,9 @@ static unsigned long page_count = 0;
 static long frontswap_loads = 0;
 static unsigned long last_user = 0, last_system = 0, last_idle = 0, last_iowait = 0, last_other = 0;
 static unsigned long stat_user = 0, stat_system = 0, stat_idle = 0, stat_iowait = 0, stat_other = 0;
+static int random_fill_pages = 1;
+static void *base_page = NULL;
+static int recent_pages = 64;
 
 void *cpu_worker(void *arg)
 {
@@ -55,18 +57,18 @@ void *random_page_worker(void *arg)
 void *recent_page_worker(void *arg)
 {
 	unsigned long *counter = (unsigned long *)arg;
-	int r, v;
+	int i = page_count;
 	while (!mem_recent_exit) {
 		if (mem_recent_pause)
 			sleep(1);
 		else {
-			r = rand() % RECENT_PAGES;
-			r = page_count - 1 - r;
-			if (r < 0)
-				r = 0;
-			v = (*counter) % 0xff;
-			memset(pages[r], v, 1);
+			if (i >= page_count)
+				i = page_count - (recent_pages * 1024);
+			if (i < 0)
+				i = 0;
+			memset(pages[i], (*counter) % 0xff, 1);
 			(*counter)++;
+			i++;
 		}
 	}
 }
@@ -80,13 +82,27 @@ void start_cpu_workers(int cpus, unsigned long counters[])
 		pthread_create(&workers[c], NULL, &cpu_worker, &counters[c]);
 }
 
+void create_base_page(size_t page_size)
+{
+	base_page = malloc(page_size);
+	int i = 0, fillsize = 0;
+	char *pg = (char *)base_page;
+	switch (random_fill_pages) {
+	case 1: fillsize = (page_size/2) - 64; break;
+	case 2: fillsize = page_size; break;
+	default: fillsize = 1; /* force allocation of phys page */ break;
+	}
+	for (; i<fillsize; i++)
+		pg[i] = rand() % 0xff;
+}
+
 unsigned long alloc_pages(void *pages[], size_t page_size, unsigned long n)
 {
 	unsigned long p = 0, total = (n / page_size);
 	for (; p<total; p++) {
 		pages[p] = malloc(page_size);
 		if (pages[p])
-			memset(pages[p], 1, 1); /* force allocation of phys page */
+			memcpy(pages[p], base_page, page_size); /* force allocation of phys page */
 		else
 			break; /* ENOMEM */
 	}
@@ -144,6 +160,41 @@ void get_stat_data()
 	last_system = tmp_system;
 	last_idle = tmp_idle;
 	last_iowait = tmp_iowait;
+}
+
+int get_zswap_max_pool()
+{
+	FILE *fp = fopen("/sys/module/zswap/parameters/max_pool_percent", "r");
+	char *line = NULL, *tmp;
+	size_t len = 0;
+	ssize_t read;
+	int max = 0;
+
+	if (!fp)
+		return 0;
+
+	if ((read = getline(&line, &len, fp)) >= 0) {
+		max = atoi(line);
+		free(line);
+	}
+
+	fclose(fp);
+
+	return max;
+}
+
+void set_zswap_max_pool(int max)
+{
+	FILE *fp = fopen("/sys/module/zswap/parameters/max_pool_percent", "w");
+	char s[64];
+
+	if (!fp)
+		return;
+
+	snprintf(s, 64, "%d", max);
+	fputs(s, fp);
+
+	fclose(fp);
 }
 
 void get_meminfo_vals(char *keys[], unsigned long vals[])
@@ -244,13 +295,29 @@ unsigned long inline calc_time_diff_ms(struct timespec before, struct timespec a
 
 double inline pct(unsigned long n, unsigned long d)
 {
+	if (d == 0)
+		return 0;
 	return (double)(n * 100) / (double)d;
 }
 
 double inline adj_counter_pct(unsigned long counter, unsigned long baseline, unsigned long ms) {
-	double adj = (double)ms / (double)(WORK_SLEEP_TIME * 1000);
+	double adj = (double)ms / (double)(work_sleep_time * 1000);
 	double bl_new = (double)baseline * adj;
 	return (double)(counter * 100) / bl_new;
+}
+
+void show_help(char *argv[])
+{
+	printf("Usage: %s [opts]\n", argv[0]);
+	printf("  -nocpu          Do not test the CPU use\n");
+	printf("  -norandom       Do not test random page accesses\n");
+	printf("  -norecent       Do not test recently allocated page accesses\n");
+	printf("  -nofillpages    Zero-fill allocated pages\n");
+	printf("  -halffillpages  Almost half fill allocated pages with random data\n");
+	printf("  -fillpages      Completely fill allocated pages with random data\n");
+	printf("  -worktime SECS  Sleep for SECS seconds between each new allocation round [default %d]\n",work_sleep_time);
+	printf("  -recentpages N  Touch Nk most recent pages with the recent page test thread [default %dk]\n",recent_pages);
+	printf("  -help           This help\n");
 }
 
 void main(int argc, char *argv[])
@@ -259,7 +326,6 @@ void main(int argc, char *argv[])
 	size_t page_size = (size_t)sysconf(_SC_PAGESIZE);
 	unsigned long phys_pages = sysconf(_SC_PHYS_PAGES);
 	unsigned long max_pages = (phys_pages * 2); /* 2x is arbitrary */
-	pages = calloc(max_pages, sizeof(void*));
 	unsigned long cpu_counters[cpus];
 	unsigned long mem_random_counter, mem_recent_counter;
 	pthread_t mem_random_worker, mem_recent_worker;
@@ -279,9 +345,33 @@ void main(int argc, char *argv[])
 			no_random = 1;
 		else if (!strcmp("-norecent", argv[a]))
 			no_recent = 1;
+		else if (!strcmp("-nofillpages", argv[a]))
+			random_fill_pages = 0;
+		else if (!strcmp("-halffillpages", argv[a]))
+			random_fill_pages = 1;
+		else if (!strcmp("-fillpages", argv[a]))
+			random_fill_pages = 2;
+		else if (!strcmp("-worktime", argv[a]))
+			work_sleep_time = atoi(argv[a++]);
+		else if (!strcmp("-recentpages", argv[a]))
+			recent_pages = atoi(argv[a++]);
+		else if (!strcmp("-help", argv[a]) || !strcmp("-h", argv[a])) {
+			show_help(argv);
+			return;
+		} else {
+			printf("Invalid opt : %s\n",argv[a]);
+			show_help(argv);
+			return;
+		}
 	}
 
+	pages = calloc(max_pages, sizeof(void*));
+
+	create_base_page(page_size);
+
 	printf("Version %d\n",VERSION);
+
+	printf("zswap max pool pct %d\n", get_zswap_max_pool());
 
 	printf("Getting baseline numbers\n");
 	fflush(NULL);
@@ -289,18 +379,18 @@ void main(int argc, char *argv[])
 	if (!no_cpu) {
 		start_cpu_workers(cpus, cpu_counters);
 		bzero(cpu_counters, cpus * sizeof(unsigned long));
-		sleep(WORK_SLEEP_TIME);
+		sleep(work_sleep_time);
 		bl_cpu_count = calc_counter(cpu_counters, cpus);
 
 		cpu_workers_pause = 1;
 	}
 
-	page_count += alloc_pages(&pages[page_count], page_size, page_size * RECENT_PAGES);
+	page_count += alloc_pages(&pages[page_count], page_size, page_size * recent_pages * 1024);
 
 	if (!no_random) {
 		pthread_create(&mem_random_worker, NULL, &random_page_worker, &mem_random_counter);
 		mem_random_counter = 0;
-		sleep(WORK_SLEEP_TIME);
+		sleep(work_sleep_time);
 		bl_random_count = mem_random_counter;
 
 		mem_random_pause = 1;
@@ -309,7 +399,7 @@ void main(int argc, char *argv[])
 	if (!no_recent) {
 		pthread_create(&mem_recent_worker, NULL, &recent_page_worker, &mem_recent_counter);
 		mem_recent_counter = 0;
-		sleep(WORK_SLEEP_TIME);
+		sleep(work_sleep_time);
 		bl_recent_count = mem_recent_counter;
 	}
 
@@ -323,11 +413,17 @@ void main(int argc, char *argv[])
 	mem_random_pause = 0;
 	sleep(1);
 
+	switch (random_fill_pages) {
+	case 1: printf("Allocated pages half-filled with random numbers\n"); break;
+	case 2: printf("Allocated pages filled with random numbers\n"); break;
+	default: printf("Allocated pages zero-filled (except 1 byte to force physical page allocation)\n"); break;
+	}
 	printf("All mem units %% of total physical mem\n");
 	printf("CPU and MEMORY units %% of baseline measurement\n");
 	printf("Allocation time units ms\n");
 	printf("Alloc period is when new memory is being allocated\n");
-	printf("Measure period is %d secs sleep delay to measure counters\n", WORK_SLEEP_TIME);
+	printf("Recent page testing %dk most recently allocated pages\n",recent_pages);
+	printf("Measure period is %d secs sleep delay to measure counters\n", work_sleep_time);
 	printf("\n");
 	printf("               |                     Measure Period                        |              Alloc Period                 |\n");
 	printf("total|used|swap| CPU |  MEMORY |  MEMORY |  zswap  | user| sys | idle| iowt| alloc | CPU |  MEMORY |  MEMORY |  zswap  |\n");
@@ -365,7 +461,7 @@ void main(int argc, char *argv[])
 		mem_random_counter = 0;
 		mem_recent_counter = 0;
 		get_stat_data();
-		sleep(WORK_SLEEP_TIME);
+		sleep(work_sleep_time);
 		get_stat_data();
 		printf(" %3.0f | %2.0f | %2.0f | %3.0f | %7.3f | %7.3f | %7ld |%4ld |%4ld |%4ld |%4ld | %5ld | %3.0f | %7.3f | %7.3f | %7ld |\n",
 					 used_mem,
